7938509d7bc4514989c62bb19d565952e4f203f5
Zbigniew Stasiak <stasiakzb@gmail.com>
2018-03-22 13:26:32 +0100

    'initializing'

--- app.js
+++ app.js
@@ -5,5 +5,16 @@ const mysql = require('mysql');
 
 const app = express();
 
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 console.log('Hello world');
 console.log('Hello world second time');
+console.log('is it working?');
+
+app.listen(app.get('port'), () => {
+  console.log(`Node app is running on port ${app.get('port')}`);
+})
