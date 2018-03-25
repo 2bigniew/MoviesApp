9f07549b4d4e5d6f02db20739fa1ad2e739fa974
Zbigniew Stasiak <stasiakzb@gmail.com>
2018-03-25 16:19:43 +0200

    'Initializing'

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -7,60 +7,78 @@ const con = require('../../database/connection');
 const queries = require('../../database/queries');
 
 router.post('/:id&:comment', (req, res, next) => {
-  const movieId = req.params.id;
-  const comment = req.params.comment;
-  con.query(queries.addComment(comment, movieId), (err, result) => {
-    if (err) res.status(500).json({
-      message: 'SQL Error. Make sure you passed corect movieId',
-      error: err.errno
-    });
-    res.status(201).json({
-      message: `Comment was added successfuly to movie(movieId: ${req.params.id})`,
-      comment: req.params.comment
-    });
-  });
+  pool.getConnection((err, con) => {
+    if (err) {
+      res.send('Error ocured');
+    } else {
+      const movieId = req.params.id;
+      const comment = req.params.comment;
+      con.query(queries.addComment(comment, movieId), (err, result) => {
+        if (err) res.status(500).json({
+          message: 'SQL Error. Make sure you passed corect movieId',
+          error: err.errno
+        });
+        res.status(201).json({
+          message: `Comment was added successfuly to movie(movieId: ${req.params.id})`,
+          comment: req.params.comment
+        });
+        con.release();
+      });
+    }
 });
 
 router.get('/', (req, res, next) => {
-  con.query(queries.showAllComments, (err, result) => {
-    if (err) res.status(500).json({
-      message: 'SQL Error',
-      error: err.errno
-    });
-    //res.send(JSON.stringify(results));
-    //res.send('<h1>Movies comments</h1>'+result.map( x => `<p>${JSON.stringify(x)}</p>`).join(''));
-    if (result.length === 0){
-      res.status(200).json({
-        message: 'There is not any comment in database'
-      });
+  pool.getConnection((err, con) => {
+    if (err) {
+      res.send('Error ocured');
     } else {
-      res.status(200).json({
-        message: 'All comments from database',
-        data: result
+      con.query(queries.showAllComments, (err, result) => {
+        if (err) res.status(500).json({
+          message: 'SQL Error',
+          error: err.errno
+        });
+        //res.send(JSON.stringify(results));
+        //res.send('<h1>Movies comments</h1>'+result.map( x => `<p>${JSON.stringify(x)}</p>`).join(''));
+        if (result.length === 0){
+          res.status(200).json({
+            message: 'There is not any comment in database'
+          });
+        } else {
+          res.status(200).json({
+            message: 'All comments from database',
+            data: result
+          });
+        }
+        con.release();
       });
     }
-  });
 });
 
 router.get('/:find', (req, res, next) => {
-  con.query(queries.showComment(req.query.Id), (err, result) => {
-    if (err) res.status(500).json({
-      message: 'SQL Error',
-      error: err.errno
-    });
-
-    if (result.length === 0) {
-      res.status(200).json({
-        message: `There is not any comment to movie where MovieId=${req.query.Id}`
-      });
+  pool.getConnection((err, con) => {
+    if (err) {
+      res.send('Error ocured');
     } else {
-      res.status(200).json({
-        message: `result of search where MovieID = '${req.query.Id}' in comments database`,
-        data: result
-      })
+      con.query(queries.showComment(req.query.Id), (err, result) => {
+        if (err) res.status(500).json({
+          message: 'SQL Error',
+          error: err.errno
+        });
+
+        if (result.length === 0) {
+          res.status(200).json({
+            message: `There is not any comment to movie where MovieId=${req.query.Id}`
+          });
+        } else {
+          res.status(200).json({
+            message: `result of search where MovieID = '${req.query.Id}' in comments database`,
+            data: result
+          })
+        }
+        //res.send(`<h1>Movie ID: ${req.params.movieId} comment: </h1><p>${JSON.stringify(result[0])}</p>`);
+        con.release();
+      });
     }
-    //res.send(`<h1>Movie ID: ${req.params.movieId} comment: </h1><p>${JSON.stringify(result[0])}</p>`);
-  })
-})
+});
 
 module.exports = router;
--- api/routes/movies.js
+++ api/routes/movies.js
@@ -35,50 +35,61 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:find', (req, res, next) => {
-  con.query(queries.showMovie(req.query.Title), (err, result) => {
-    if (err) res.status(500).json({
-      message: 'SQL Error',
-      error: err.errno
-    });
-
-    if (result.length === 0){
-      res.status(200).json({
-          message: `There is not movie '${req.query.Title}' in database`
-      });
+  pool.getConnection((err, con) => {
+    if (err) {
+      res.send('Error ocured');
     } else {
-      res.status(200).json({
-        message: `result of search '${req.query.Title}'  in movies database`,
-        data: result
+      con.query(queries.showMovie(req.query.Title), (err, result) => {
+        if (err) res.status(500).json({
+          message: 'SQL Error',
+          error: err.errno
+        });
+
+        if (result.length === 0){
+          res.status(200).json({
+              message: `There is not movie '${req.query.Title}' in database`
+          });
+        } else {
+          res.status(200).json({
+            message: `result of search '${req.query.Title}'  in movies database`,
+            data: result
+          });
+        }
+        con.release();
       });
     }
-  });
 });
 
 router.get('/list/:sort', (req, res, next) => {
-  if(parseInt(req.query.Option) <=6) {
-    con.query(queries.orderMovies(req.query.Option), (err, result) => {
-      if(err) res.status(500).json({
-        message: 'SQL Error',
-        error: err.errno
-      });
+  pool.getConnection((err, con) => {
+    if (err) {
+      res.send('Error ocured');
+    } else {
+      if(parseInt(req.query.Option) <=6) {
+        con.query(queries.orderMovies(req.query.Option), (err, result) => {
+          if(err) res.status(500).json({
+            message: 'SQL Error',
+            error: err.errno
+          });
 
-      if (result.length === 0) {
-        res.status(200).json({
-          message: 'There is not any movie in database'
+          if (result.length === 0) {
+            res.status(200).json({
+              message: 'There is not any movie in database'
+            });
+          } else {
+            res.status(200).json({
+              message: "All sorted movies from database",
+              data: result
+            });
+          }
+          con.release();
         });
       } else {
         res.status(200).json({
-          message: "All sorted movies from database",
-          data: result
+          message: 'No option available. Option should equal number between 1-6.'
         });
       }
-    });
-
-  } else {
-    res.status(200).json({
-      message: 'No option available. Option should equal number between 1-6.'
-    });
-  }
+    }
 });
 
 //   if(req.query.Option <=6) {
@@ -110,27 +121,33 @@ router.get('/list/:sort', (req, res, next) => {
 // });
 
 router.post('/:title', (req, res, next) => {
-  const movieTitle = helpers.validateTitle(req.params.title);
-  axios.get('http://www.omdbapi.com/?s='+movieTitle+'&apikey=b3a0b8c6')
-    .then(response => {
-      con.query(queries.addMovie(
-        response.data.Search[0].Title,
-        response.data.Search[0].Year,
-        response.data.Search[0].imdbID,
-        response.data.Search[0].Type,
-        response.data.Search[0].Poster
-      ), (err, result) => {
-        if(err) throw err;
-      })
-      res.status(201).json({
-        message: `'${response.data.Search[0].Title}' movie was successfuly added to the database`,
-        movieData: response.data.Search[0]
-      });
-    })
-    .catch(error => res.status(404).json({
-      error: '404 Not Found',
-      message: `Cannot find movie '${movieTitle}' in OMDb API`
-    }));
+  pool.getConnection((err, con) => {
+    if (err) {
+      res.send('Error ocured');
+    } else {
+      const movieTitle = helpers.validateTitle(req.params.title);
+      axios.get('http://www.omdbapi.com/?s='+movieTitle+'&apikey=b3a0b8c6')
+        .then(response => {
+          con.query(queries.addMovie(
+            response.data.Search[0].Title,
+            response.data.Search[0].Year,
+            response.data.Search[0].imdbID,
+            response.data.Search[0].Type,
+            response.data.Search[0].Poster
+          ), (err, result) => {
+            if(err) throw err;
+          });
+          res.status(201).json({
+            message: `'${response.data.Search[0].Title}' movie was successfuly added to the database`,
+            movieData: response.data.Search[0]
+          });
+        })
+        .catch(error => res.status(404).json({
+          error: '404 Not Found',
+          message: `Cannot find movie '${movieTitle}' in OMDb API`
+        }));
+        con.release();
+    }
 });
 
 module.exports = router;
